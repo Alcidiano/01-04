19603534e05067172755a9027b8c4282c43f5640
ALCIDIANO FERREIRA SOARES <alcidiano.fsoares@senacsp.edu.br>
2024-04-01 14:35:53 -0300

    01-04-js

---
+++ Documents/01-04/4-revisão/js/script.js
@@ -0,0 +1,54 @@
+// console.log("Óla Mundo!")
+// console.log('Óla Mundo2!')
+// console.log(`Olá Mundo3!`)
+
+// let nome = `Alcidiano`
+// const idade = 33
+// var profissao = 'programador'
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+// console.log(profissao)
+
+// console.log(nome + idade +profissao)
+
+
+// console.log(`${nome} ${idade} ${profissao}`)
+// console.log(`Usuário: ${nome} Idade: ${idade} Profissão: ${profissao}`)
+
+// var idade = parseInt(prompt("Digite sua idade"))
+// alert("Você tem " + idade + " anos")
+
+ 
+// alert("Daqui a 10anos voçÊ terá" + idade + " anos")
+
+// var idade2 = idade + 10
+
+// alert("Daqui aa 10 anos voçê terá " + idade2 + " anos")
+
+// let nome = prompt("Digite seu nome")
+// let sobrenome = prompt("Sobrenome")
+
+// alert(`O nome completo é: ${nome} ${sobrenome}`)
+
+// let idade = parseInt(prompt("Idade"))
+
+// if(idade >= 18){
+//     alert(`Pode entrar
+//     `)
+// }else{
+// alert(`Não pode entrar`)
+// }
+
+let idade = parseInt(prompt("Idade"))
+
+if(idade >= 18){
+    alert(`Pode entrar
+    `)
+}else if(idade >=15 && idade<18){
+    alert(`Pode entrar com os pais`)
+}
+else{
+alert(`Não pode entrar`)
+}
+
