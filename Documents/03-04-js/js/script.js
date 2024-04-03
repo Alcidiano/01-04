7ff85c2c1c121b6b470d3fc5cfe4c42db24214f4
ALCIDIANO FERREIRA SOARES <alcidiano.fsoares@senacsp.edu.br>
2024-04-03 15:03:09 -0300

    03-04

---
+++ Documents/03-04-js/js/script.js
@@ -0,0 +1,103 @@
+// Exemplo-01
+
+// function minhaFuncao(){
+//     console.log(`Testando`)
+// }
+
+// minhaFuncao()
+// minhaFuncao()
+
+// Exemplo-02
+
+// const minhaFuncaoEmVariavel = function(txt){
+//     console.log(`Imprimindo: ${txt}`)
+// }
+
+// minhaFuncaoEmVariavel(`Sapato`)
+// minhaFuncaoEmVariavel(`colher`)
+// minhaFuncaoEmVariavel(`5+5`)
+
+// Exemplo-03
+
+// const a = 10;
+// const b = 20;
+// const c = 30;
+// const d = 40;
+
+// function soma(n1, n2){
+//     console.log(`A soma entre ${n1} + ${n2} é = ${n1 + n2}`)
+//     console.log(`A sub entre ${n1} - ${n2} é = ${n1 - n2}`)
+//     console.log(`A div entre ${n1} / ${n2} é = ${n1 / n2}`)
+//     console.log(`A mult entre ${n1} * ${n2} é = ${n1 * n2}`)
+//     return
+// }
+// soma(a,b)
+// soma(c,b)
+// soma(d,a)
+
+// Exemplo-04
+
+// function raizQuadrada (numero){
+//     console.log(numero * numero)
+// }
+// raizQuadrada(4)
+
+// 4.1
+
+// function raizQuadrada (numero){
+//     return numero * numero
+    
+// }
+// console.log(raizQuadrada(6))
+
+// 4.2
+
+// const raizQuadrada = (numero) => {
+//     return numero * numero
+// }
+// console.log(raizQuadrada(5))
+
+// Exemplo-05
+
+// const testeArrow = () => {
+//     console.log(`Essa é uma arrow fonction`)
+// }
+
+// testeArrow()
+
+// 5.1
+
+// const parOuImpar = (n) => {
+//     if(n % 2 === 0){
+//         console.log(`${n} é par`)
+//         return
+//     }
+//     console.log(`${n} é impar`)
+// }
+// parOuImpar(2)
+
+// Exemplo-06
+
+// const lista = [1, 2, 3, 4, 5,6, 7, 8]
+
+// console.log(lista)
+// console.log(typeof lista)
+
+// console.log(lista[1])
+// console.log(lista[0])
+// console.log(lista.length)
+
+// // const lista2 = ["Gol", "Stilo", "Fusca", "Golf", "Opala", "Punto"] 
+// // // console.log(lista2.length)
+// // for(let i = 0; i < lista2.length; i++){
+// //     console.log(`Elemento da lista2: ${lista2[i]}`)
+// }
+
+
+// const lista = [1, 2, 3, 4, 5] 
+
+//     for(let i = 0; i < lista.length; i++){
+//     console.log(`Elemento da lista: ${lista[i]}`)
+// }
+
+// console.log(lista2.length)
